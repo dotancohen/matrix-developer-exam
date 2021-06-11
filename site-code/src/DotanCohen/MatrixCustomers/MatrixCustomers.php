fb70c2e913c95f4ad089001f8fac0aa2eb4c1cd2
Dotan Cohen <github-com-2010.04@dotancohen.com>
2021-06-11 19:18:06 +0300

    Dynamically run methods

--- site-code/src/DotanCohen/MatrixCustomers/MatrixCustomers.php
+++ site-code/src/DotanCohen/MatrixCustomers/MatrixCustomers.php
@@ -8,6 +8,7 @@ class MatrixCustomers {
 	{
 		// Select a method based on inputs
 		var_dump($route);
+		$method = "beer";
 		
 		
 		
@@ -17,6 +18,22 @@ class MatrixCustomers {
 		}
 		$this->classMethod = $method;
 		$this->requestBody = $requestBody;
+	}
+	
+	
+	public function run()
+	{
+		if (!method_exists($this, $this->classMethod)) {
+			throw new \Exception("Invalid Method");
+		}
+		
+		$this->{$this->classMethod}();
+	}
+	
+	
+	public function beer()
+	{
+		echo "BEER";
 	}
 	
 }
