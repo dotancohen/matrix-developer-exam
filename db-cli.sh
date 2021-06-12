a59d818ac59242e14ff166c95b327fb675ca4a72
Dotan Cohen <github-com-2010.04@dotancohen.com>
2021-06-12 12:25:04 +0300

    Document bash scripts

--- db-cli.sh
+++ db-cli.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Return CLI command to connect to MySQL (e.g. for running script files), and additionally connect.
+
 echo mysql -h $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' matrix-docker_mysql_1) -u root -p$(grep MYSQL_PASSWORD site-code/.env | awk  '{split($0,a,"="); print a[2]}') matrix
 mysql -h $(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' matrix-docker_mysql_1) -u root -p$(grep MYSQL_PASSWORD site-code/.env | awk  '{split($0,a,"="); print a[2]}') matrix
--- server-address.sh
+++ server-address.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# Return base address of server running in Docker container
+
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' matrix-docker_nginx_1
