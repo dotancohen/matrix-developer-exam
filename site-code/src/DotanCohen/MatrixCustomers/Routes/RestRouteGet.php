b30edf52265297599263bddad8914eb0d7821b62
Dotan Cohen <github-com-2010.04@dotancohen.com>
2021-06-12 19:57:13 +0300

    Implement customerIdGov route

--- site-code/src/DotanCohen/MatrixCustomers/Routes/RestRouteGet.php
+++ site-code/src/DotanCohen/MatrixCustomers/Routes/RestRouteGet.php
@@ -2,18 +2,31 @@
 
 namespace DotanCohen\MatrixCustomers\Routes;
 
+use DotanCohen\MatrixCustomers\Database\Customer;
+
 class RestRouteGet extends RestRoute {
-	
+
+
 	public function customerPhone($params=[], $body=null) : void
 	{
 		// TODO
 		echo "GET PHONE";
 	}
-	
+
+
 	public function customerIdGov($params=[], $body=null) : void
 	{
-		// TODO
-		echo "GET ID GOV";
+		if ( count($params)<1 ) {
+			$resp = ['error' => 'Missing ID'];
+			self::response($resp, 400);
+		}
+
+		$id_gov = $params[0];
+		$c = Customer::getByGovId($id_gov);
+
+		$resp = ['customer' => $c->toPublic()];
+		$code = is_null($c->id) ? 404 : 200;
+		self::response($resp, $code);
 	}
 
 }
