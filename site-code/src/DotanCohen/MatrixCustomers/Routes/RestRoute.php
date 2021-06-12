cc329f95bf61f1d0accd30303d55cdd1c6bda69c
Dotan Cohen <github-com-2010.04@dotancohen.com>
2021-06-12 14:00:24 +0300

    Add method for providing a uniform response to the client

--- site-code/src/DotanCohen/MatrixCustomers/Routes/RestRoute.php
+++ site-code/src/DotanCohen/MatrixCustomers/Routes/RestRoute.php
@@ -4,6 +4,22 @@ namespace DotanCohen\MatrixCustomers\Routes;
 
 class RestRoute {
 
+	/**
+	 * Provide a uniform response to the client
+	 * 
+	 * @param array $response
+	 */
+	protected function response(array $response) : void
+	{
+		header('content-type: application/json');
+		header('access-control-allow-credentials: true');
+		//header('access-control-allow-methods: GET, POST, HEAD, OPTIONS, DELETE');
+		header('access-control-allow-methods: GET, POST, HEAD, DELETE');
+		header('access-control-allow-origin: *');
+		header('access-control-max-age: 300');
+		header('cache-control: no-cache, no-store');
 
+		echo json_encode($response);
+	}
 
 }
