ce4f362c8c6673276cb55f3f51aef1e3ebd84511
Dotan Cohen <github-com-2010.04@dotancohen.com>
2021-06-12 18:02:51 +0300

    Implement storing customers via API (not including phone numbers)

--- site-code/src/DotanCohen/MatrixCustomers/Routes/RestRoutePost.php
+++ site-code/src/DotanCohen/MatrixCustomers/Routes/RestRoutePost.php
@@ -2,12 +2,23 @@
 
 namespace DotanCohen\MatrixCustomers\Routes;
 
+use DotanCohen\MatrixCustomers\Database\Customer;
+
 class RestRoutePost extends RestRoute {
 	
 	public function customer(?array $params=[], ?\stdClass $body=null) : void
 	{
-		// TODO
-		echo "Post customer";
+		$c = new Customer();
+		$c->id_gov = $body->id_gov;
+		$c->name_first = $body->name_first;
+		$c->name_last = $body->name_last;
+		$c->date_birth = new \DateTime($body->date_birth);
+		$c->sex = $body->sex;
+		$c->phones = $body->phones;
+		$cust_id = $c->save();
+
+		$resp = ['customer_id' => $cust_id];
+		self::response($resp);
 	}
 
 }
